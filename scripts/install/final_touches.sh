d83fa51157f84e2ce77f8c5ae9039ee3dd1e899b
Angela Mancini <angela.mancini@sage.com>
2016-06-06 14:27:14 -0400

    fixed final touches script

--- scripts/install/final_touches.sh
+++ scripts/install/final_touches.sh
@@ -1,21 +1,47 @@
-echo 'copy bash prompt'
-cp /tmp/custom_prompt.sh /etc/profile.d/custom_prompt.sh
+echo 'set custom bash prompt'
+cat > /etc/profile.d/custom_prompt.sh << EOF
+# Place this file in /etc/profile.d and wonderful things will happen
+
+# Define some colors
+default='\[\033[00m\]'
+bold='\[\033[01m\]'
+red='\[\033[31m\]'
+green='\[\033[32m\]'
+yellow='\[\033[33m\]'
+blue='\[\033[34m\]'
+magenta='\[\033[35m\]'
+cyan='\[\033[36m\]'
+white='\[\033[37m\]'
+
+# Git branch in prompt.
+parse_git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+# Color ls
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
+# Create a nice colorful command prompt
+export PS1="${default}\u@\h ${bold}${blue}\W${green}\$(parse_git_branch)${default} $ "
+
+# Aliases go here
+alias reload='source ~/.bash_profile'
+alias ls='ls -alh --color=always'
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
+alias rs='rails server -b 0.0.0.0'
+EOF
+
+echo 'cloning random-bits repo to /tmp'
+cd /tmp
+git clone https://github.com/angelamancini/random-bits.git
 
 echo 'copy in vimrc'
-cp /tmp/my-vimrc /home/vagrant/.vimrc
+cp /tmp/random-bits/.vimrc /home/vagrant/.vimrc
 
 echo 'copy in tmux.conf'
-cp /tmp/my-tmux.conf /home/vagrant/.tmux.conf
+cp /tmp/random-bits/.tmux.conf /home/vagrant/.tmux.conf
 
-echo 'handle keys'
-echo 'copy vagrant public key'
-cp /tmp/vagrant_rsa.pub /home/vagrant/.ssh/vagrant_rsa.pub
-echo 'add public key to authorized_keys'
-cat /home/vagrant/.ssh/vagrant_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-echo 'add private key'
-echo $VAGRANT_SSH_PRIVATE_KEY > /home/vagrant/.ssh/vagrant_rsa
-echo 'cmod private key'
-chmod 400 /home/vagrant/.ssh/vagrant_rsa
-# eval "$(ssh-agent -s)"
-# echo 'add key to ssh agent'
-# ssh-add /home/vagrant/.ssh/vagrant_rsa
+rm -rf /tmp/random-bits
