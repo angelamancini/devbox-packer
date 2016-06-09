edffd1527d5089a6e232f88d7e26d90277878334
Angela Mancini <angela.mancini@sage.com>
2016-06-09 11:30:05 -0400

    removed special provisioning

--- scripts/install/final_touches.sh
+++ scripts/install/final_touches.sh
@@ -1,14 +1,8 @@
-echo 'cloning random-bits repo to /tmp'
-cd /tmp
-git clone https://github.com/angelamancini/random-bits.git
-
 echo 'copy in vimrc'
-cp /tmp/random-bits/.vimrc /home/vagrant/.vimrc
+cp /tmp/.vimrc /home/vagrant/.vimrc
 
 echo 'copy in tmux.conf'
-cp /tmp/random-bits/.tmux.conf /home/vagrant/.tmux.conf
+cp /tmp/.tmux.conf /home/vagrant/.tmux.conf
 
 echo 'set custom bash prompt'
-cp /tmp/random-bits/custom_prompt.sh /etc/profile.d/custom_prompt.sh 
-
-rm -rf /tmp/random-bits
+cp /tmp/custom_prompt.sh /etc/profile.d/custom_prompt.sh
