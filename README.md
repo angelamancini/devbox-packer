ced6330df8c85ac018a7bf070ead63500f919ef7
Angela Mancini <angeladmancini@gmail.com>
2016-06-26 02:39:43 -0400

    added quick vim install and colors

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 ## Packer files to build CentOS 7.2 and Vagrant boxes for VirtualBox
 
+Installed for development goodness:
+
+- PostgreSQL v9.3.13
+- Redis v2.8.19
+- RVM v1.27.0 with Ruby v2.1.3
+- Git v2.9.0
+- Tmux v2.1
+- Vim v7.4
+
 Tested with:
 
 [Packer](https://packer.io/) v0.10.1
+
+[Ansible](https://www.ansible.com) v2.0.0.2
 
 [VirtualBox](https://www.virtualbox.org/) v5.0.20
 
--- scripts/install/final_touches.sh
+++ scripts/install/final_touches.sh
@@ -1,8 +1,14 @@
 echo 'copy bash prompt'
 cp /tmp/custom_prompt.sh /etc/profile.d/custom_prompt.sh
 
+echo 'install vim'
+sudo yum install -y vim
+
 echo 'copy in vimrc'
-cp /tmp/my-vimrc /home/vagrant/.vimrc
+cp /tmp/.vimrc /home/vagrant/.vimrc
+
+echo 'copy in custom vim colors'
+cp /tmp/codeschool.vim /home/vagrant/.vim/colors/
 
 echo 'copy in tmux.conf'
-cp /tmp/my-tmux.conf /home/vagrant/.tmux.conf
+cp /tmp/.tmux.conf /home/vagrant/.tmux.conf
